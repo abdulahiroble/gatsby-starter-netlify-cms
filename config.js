7aecfbf324b72a9aab396032f29c72bf806e901f
abdulahiroble <abdulahi_2610@hotmail.com>
2020-08-02 04:40:46 +0200

    config file created

---
+++ config.js
@@ -0,0 +1,40 @@
+module.exports = {
+  siteTitle:
+    "Bedste Gamer Laptop til prisen 2020 - Find de bedste gamer bærbare her", // <title>
+  shortSiteTitle: "bedste gamer bærbar", // <title> ending for posts and pages
+  siteDescription: "Find din næste gamer bærbar her",
+  siteUrl: "https://bedstegamerlaptop.dk/",
+  // pathPrefix: "",
+  // siteImage: "preview.jpg",
+  siteLanguage: "de",
+
+  /* author */
+  // authorName: "greg lobinski",
+  // authorTwitterAccount: "greglobinski",
+
+  /* info */
+  headerTitle: "bedste gamer bærbar",
+  // headerSubTitle: "presents another one GatsbyJS starter",
+
+  /* manifest.json */
+  manifestName: "Bedste gamer laptop til prisen 2020",
+  manifestShortName: "bedste gamer laptop", // max 12 characters
+  manifestStartUrl: "/index.html",
+  manifestBackgroundColor: "white",
+  manifestThemeColor: "#666",
+  manifestDisplay: "standalone",
+
+  // gravatar
+  // Use your Gravatar image. If empty then will use src/images/jpg/avatar.jpg
+  // Replace your email adress with md5-code.
+  // Example https://www.gravatar.com/avatar/g.strainovic@gmail.com ->
+  // gravatarImgMd5: "https://www.gravatar.com/avatar/1db853e4df386e8f699e4b35505dd8c6",
+  gravatarImgMd5: "",
+
+  // social
+  authorSocialLinks: [
+    // { name: "github", url: "https://github.com/greglobinski" },
+    // { name: "twitter", url: "https://twitter.com/greglobinski" },
+    // { name: "facebook", url: "http://facebook.com/greglobinski" }
+  ],
+};
